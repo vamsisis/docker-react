6ce7f004e4313a9ea75978e6494b1b225103f81e
vamsi siddineni <60202191+vamsisis@users.noreply.github.com>
2024-12-17 14:18:03 -0600

    Update jdbcalerts.go

--- src/jdbcalerts.go
+++ src/jdbcalerts.go
@@ -2,3 +2,39 @@
   {{ if gt (len .Alerts.Firing) 0 }}🚨 Firing alert(s): JDBC Connection Usage Alert: {{ .CommonLabels.alert_name }} - {{ .CommonLabels.job }} - {{ .CommonLabels.value }}%{{ end }}
   {{ if gt (len .Alerts.Resolved) 0 }}✅ Resolved alert(s): JDBC Connection Usage Alert: {{ .CommonLabels.alert_name }} - {{ .CommonLabels.job }} - {{ .CommonLabels.value }}%{{ end }}
 {{ end }}
+
+=============================================================
+
+{{ define "CustomBodyforAlerting" -}}
+{{ if or (gt (len .Alerts.Firing) 0) (gt (len .Alerts.Resolved) 0) -}}
+
+{{ if gt (len .Alerts.Firing) 0 -}}
+**Firing Alerts**
+{{ range .Alerts.Firing -}}
+**Alert Details:**
+- Alert Name: {{ .Labels.alert_name }}
+- Job Name: {{ .Labels.job }}
+- JDBC Connection Usage: {{ .Value }}%
+
+{{ else -}}
+- Description: {{ .Annotations.description }}
+{{ end -}}
+{{ end -}}
+
+{{ if gt (len .Alerts.Resolved) 0 -}}
+**Resolved Alerts**
+{{ range .Alerts.Resolved -}}
+**Alert Details:**
+- Alert Name: {{ .Labels.alert_name }}
+- Job Name: {{ .Labels.job }}
+- JDBC Connection Usage: {{ .Value }}%
+
+{{ else -}}
+- Description: {{ .Annotations.description }}
+{{ end -}}
+{{ end -}}
+
+{{ else -}}
+No active alerts.
+{{ end -}}
+{{ end -}}
